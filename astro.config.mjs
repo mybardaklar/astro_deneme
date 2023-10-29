1f1c2fb92d0eef2b2402ccc987859090f752542f
myB <myasarbardaklar47@gmail.com>
2023-10-29 23:22:19 +0300

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,49 @@
-import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
-import alpine from '@astrojs/alpinejs';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import compress from "astro-compress";
+import alpine from "@astrojs/alpinejs";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		assets: true,
-	},
 	build: {
-		format: 'file',
+		assetsPrefix: ".",
+		format: "file",
+		assets: "assets",
+		inlineStylesheets: "auto",
+		/* rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		}, */
+	},
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					entryFileNames: "assets/js/[name].[hash].js",
+					assetFileNames: ({ name }) => {
+						/* if (/\.(woff(2)?|ttf|otf|eot|svg)$/.test(name ?? "")) {
+							return "assets/fonts/[name]/[name].[hash][extname]";
+						} */
+
+						if (/\.(gif|jpe?g|png|svg|webp|ico)$/.test(name ?? "")) {
+							return "assets/img/[name].[hash][extname]";
+						}
+
+						if (/\.css$/.test(name ?? "")) {
+							return "assets/css/[name].[hash][extname]";
+						}
+
+						// default value
+						// ref: https://rollupjs.org/guide/en/#outputassetfilenames
+						return "assets/[name].[hash][extname]";
+					},
+				},
+			},
+		},
 	},
 	integrations: [
 		tailwind({
@@ -20,5 +52,11 @@ export default defineConfig({
 			},
 		}),
 		alpine(),
+		sanity({
+			projectId: "zjooigsf",
+			dataset: "production",
+			// Set useCdn to false if you're building statically.
+			useCdn: false,
+		}),
 	],
 });
